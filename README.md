356151220045c282efe6f7d54a894b00acebb2b6
James Zhu <zhujian0805@gmail.com>
2015-09-30 21:02:22 +0800

    mod

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # my-js
+
+[Notice]
+Since we can not execute js on linux cli, we use js(nodejs) to execute. so using nodejs
+standard functions. :)
---
+++ loop-array.js
@@ -0,0 +1,7 @@
+var fruits = new Array( "apple", "orange", "mango" );
+
+var size = fruits.length;
+
+for(i=0; i<=size; i++){
+    console.log(i);
+}
